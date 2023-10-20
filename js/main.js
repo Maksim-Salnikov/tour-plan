273736402788ea34ed2706c3ac1ef43c66b202c0
Maksim-Salnikov <salomaksim123@gmail.com>
2023-10-20 13:16:54 +0300

    fixed: Hotel slider and adaptation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.hotel-swiper', {
   // Optional parameters
   loop: true,
 
@@ -8,7 +8,7 @@ const swiper = new Swiper('.swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
 });
