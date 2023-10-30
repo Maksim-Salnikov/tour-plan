d1d573b61a10cffb2170c19ac44da7e9d30fe2cf
Maksim-Salnikov <salomaksim123@gmail.com>
2023-10-30 16:04:12 +0300

    create: added modal window

--- js/main.js
+++ js/main.js
@@ -1,36 +1,59 @@
-const hotelSwiper = new Swiper('.hotel-swiper', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const hotelSwiper = new Swiper('.hotel-swiper', {
+    // Optional parameters
+    loop: true,
+  
+    keyboard: {
+      enabled: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
+  const reviewsSwiper = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+  
+    keyboard: {
+      enabled: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+  
+  var menuButtton = document.querySelector(".menu-button");
+  menuButtton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+    .querySelector(".navbar-menu")
+    .classList.toggle("navbar-menu--visible")
+  });
 
-  keyboard: {
-    enabled: true,
-  },
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal-block__close-button")
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-});
-const reviewsSwiper = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  function openModal() {
+    var modalBackside = $(".modal-backside");
+    var modalBlock = $(".modal-block");
+    modalBackside.addClass("modal-backside--visible");
+    modalBlock.addClass("modal-block--visible");
+  }
 
-  keyboard: {
-    enabled: true,
-  },
+  function closeModal(event) {
+    event.preventDefault();
+    var modalBackside = $(".modal-backside");
+    var modalBlock = $(".modal-block");
+    modalBackside.removeClass("modal-backside--visible");
+    modalBlock.removeClass("modal-block--visible");
+  }
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
-
-var menuButtton = document.querySelector(".menu-button");
-menuButtton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-  .querySelector(".navbar-menu")
-  .classList.toggle("navbar-menu--visible")
 });
